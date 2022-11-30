8b3fb5780de686eb748da606ccf0f3e80a1ec5a0
yacanom <yacanom1@gmail.com>
2022-11-29 20:01:30 -0500

    archivo pt y logica para hacer el inverse_transform de la respuesta del modelo

--- Entrega3/API/prediction_model.py
+++ Entrega3/API/prediction_model.py
@@ -1,10 +1,13 @@
 from joblib import load
+from sklearn.preprocessing import PowerTransformer
 
 class PredictionModel:
 
     def __init__(self):
-        self.model = load("models/grid2_3.joblib")
+        self.model = load("models/RF.joblib")
+        self.pt = load("models/pt.joblib")
 
     def make_predictions(self, data):
         result = self.model.predict(data)
+        result = (self.pt.inverse_transform(result.reshape(-1, 1)))
         return result
